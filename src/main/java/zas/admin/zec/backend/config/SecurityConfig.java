9e1ea473c6e80a433e89ad3a2d04b0054d2cb8df
kieran.schubert <schubert.kieran@gmail.com>
2025-02-06 16:24:14 +0100

    feat: allow PUT requests for conversation updates in security configuration

--- src/main/java/zas/admin/zec/backend/config/SecurityConfig.java
+++ src/main/java/zas/admin/zec/backend/config/SecurityConfig.java
@@ -46,6 +46,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST,"/api/users", "/api/conversations/**", "/api/auth").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/faq-items", "/api/settings", "/api/conversations/**").permitAll()
                         .requestMatchers(HttpMethod.DELETE, "/api/conversations/**").permitAll()
+                        .requestMatchers(HttpMethod.PUT, "/api/conversations/**").permitAll()
                         .requestMatchers("/api/admin/**", "/api/faq-items").hasAnyAuthority("ADMIN")
                         .requestMatchers(HttpMethod.POST, "/api/documents/**").authenticated()
                         .anyRequest().authenticated())
