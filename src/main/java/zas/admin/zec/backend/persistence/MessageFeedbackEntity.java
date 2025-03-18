4795cc9a223f799c78834114adb1aba9a5eb44ca
Mvinciar <matt.vincia@gmail.com>
2025-03-18 15:54:34 +0100

    migration of feedback endpoints

---
+++ src/main/java/zas/admin/zec/backend/persistence/MessageFeedbackEntity.java
@@ -0,0 +1,35 @@
+package zas.admin.zec.backend.persistence;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "chat_feedback")
+public class MessageFeedbackEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "user_uuid")
+    private String userUuid;
+
+    @Column(name = "conversation_uuid")
+    private String conversationUuid;
+
+    @Column(name = "message_uuid")
+    private String messageUuid;
+
+    @Column(name = "score")
+    private Integer score;
+
+    @Column(name = "comment")
+    private String comment;
+
+    @Column(name = "timestamp")
+    private LocalDateTime timestamp;
+}
