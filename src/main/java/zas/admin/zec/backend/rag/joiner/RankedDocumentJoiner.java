fe2b88f36b2dea849ff411abf7f89519db877a1c
Mvinciar <matt.vincia@gmail.com>
2025-06-30 09:53:29 +0200

    feat: enhance query processing by adding transformers and augmenters for improved context handling

--- src/main/java/zas/admin/zec/backend/rag/joiner/RankedDocumentJoiner.java
+++ src/main/java/zas/admin/zec/backend/rag/joiner/RankedDocumentJoiner.java
@@ -9,6 +9,8 @@ import zas.admin.zec.backend.rag.validation.SourceValidator;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class RankedDocumentJoiner implements DocumentJoiner {
 
@@ -26,6 +28,8 @@ public class RankedDocumentJoiner implements DocumentJoiner {
                 .flatMap(entry -> entry.getValue().stream()
                         .flatMap(List::stream))
                         //.filter(doc -> sourceValidator.isValidSource(entry.getKey().text(), "fr", doc)))
+                .collect(Collectors.toMap(Document::getId, Function.identity(), (existing, duplicate) -> existing))
+                .values().stream()
                 .sorted(Comparator.comparingDouble(Document::getScore).reversed())
                 .limit(topK)
                 .toList();
