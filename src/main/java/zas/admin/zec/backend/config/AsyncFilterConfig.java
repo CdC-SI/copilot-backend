8701192e479283b800ad28a9d8b20e16482d48e7
Mvinciar <matt.vincia@gmail.com>
2026-05-28 15:33:10 +0200

    Add ApplicationRunner to log filter registrations in AsyncFilterConfig

--- src/main/java/zas/admin/zec/backend/config/AsyncFilterConfig.java
+++ src/main/java/zas/admin/zec/backend/config/AsyncFilterConfig.java
@@ -3,7 +3,10 @@ package zas.admin.zec.backend.config;
 import ch.admin.zas.jweb.webstarter.observability.CorrelationIdFilter;
 import ch.admin.zas.jweb.webstarter.observability.UserMdcFilter;
 import jakarta.servlet.DispatcherType;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.web.servlet.AbstractFilterRegistrationBean;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -34,5 +37,22 @@ public class AsyncFilterConfig {
                 DispatcherType.ERROR
         );
         return registration;
+    }
+
+    @Bean
+    ApplicationRunner dumpFilterRegistrations(ApplicationContext context) {
+        return args -> context.getBeansOfType(AbstractFilterRegistrationBean.class)
+                .forEach((name, registration) -> {
+                    System.out.printf(
+                            "FilterRegistrationBean bean=%s, filterName=%s, filter=%s, async=%s, dispatcherTypes=%s, order=%s, urls=%s%n",
+                            name,
+                            registration.getFilterName(),
+                            registration.getFilter() != null ? registration.getFilter().getClass().getName() : null,
+                            registration.isAsyncSupported(),
+                            registration.determineDispatcherTypes(),
+                            registration.getOrder(),
+                            registration.getUrlPatterns()
+                    );
+                });
     }
 }
