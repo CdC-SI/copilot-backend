9427c2d866f79bff3089d64b0c4c75ff3904c086
Mvinciar <matt.vincia@gmail.com>
2026-05-28 12:17:42 +0200

    Refactor async filter configuration to register CorrelationIdFilter and UserMdcFilter

--- src/main/java/zas/admin/zec/backend/config/AsyncFilterConfig.java
+++ src/main/java/zas/admin/zec/backend/config/AsyncFilterConfig.java
@@ -1,19 +1,27 @@
 package zas.admin.zec.backend.config;
 
+import ch.admin.zas.jweb.webstarter.observability.CorrelationIdFilter;
+import ch.admin.zas.jweb.webstarter.observability.UserMdcFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.filter.DelegatingFilterProxy;
 
 @Configuration
 public class AsyncFilterConfig {
 
     @Bean
-    public FilterRegistrationBean<DelegatingFilterProxy> asyncSecurityFilter() {
-        var bean = new FilterRegistrationBean<>(new DelegatingFilterProxy("springSecurityFilterChain"));
-        bean.setAsyncSupported(true);
-        bean.setOrder(Integer.MIN_VALUE + 1);
-        bean.addUrlPatterns("/*");
-        return bean;
+    public FilterRegistrationBean<CorrelationIdFilter> correlationIdFilterRegistration(CorrelationIdFilter filter) {
+        var registration = new FilterRegistrationBean<>(filter);
+        registration.setAsyncSupported(true);
+        registration.addUrlPatterns("/*");
+        return registration;
+    }
+
+    @Bean
+    public FilterRegistrationBean<UserMdcFilter> userMdcFilterRegistration(UserMdcFilter filter) {
+        var registration = new FilterRegistrationBean<>(filter);
+        registration.setAsyncSupported(true);
+        registration.addUrlPatterns("/*");
+        return registration;
     }
 }
